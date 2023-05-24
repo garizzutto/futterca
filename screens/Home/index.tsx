a839968d6fa40cc318cfc644ee82717bd41ccd9e
Gabriel Almeida Rizzutto <garizzutto@gmail.com>
2023-05-24 16:36:15 -0300

    feat: added stack navigation and styled components

---
+++ screens/Home/index.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { View } from "./styles";
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home lalala</Text>
+    </View>
+  );
+};
+
+export default Home;
