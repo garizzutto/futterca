a656ee14f1e4546a5103d901e8784c175e2f244d
Gabriel Almeida Rizzutto <garizzutto@gmail.com>
2023-05-24 17:03:57 -0300

    fix: husky not stopping on warnings

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Home from "./screens/Home";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+import Home from './screens/Home';
 
 const Stack = createNativeStackNavigator();
 
